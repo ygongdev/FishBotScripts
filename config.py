71a1b34451e885b6def6c99e63c2f7748822c84f
Yicheng Gong <ygongdev@gmail.com>
2018-01-21 12:27:28 -0600

    removing credentials

--- config.py
+++
@@ -1,7 +0,0 @@
-# Modify these to the your data paths. I suggest creating a folder called data and putting them in there.
-CLAN_INFO_FILE_PATH = "data/tt2_clan_info.json"
-CLAN_QUEST_INFO_FILE_PATH = "data/tt2_clan_quest_info.json"
-CLAN_CODE = "gg8e6"
-GOOGLE_SPREADSHEET_ID = "1ja4_z_nd4JVqf8SYQDKazSPI0cEJ7dnUfJQW9GwPxNk"
-GOOGLE_SPREADSHEET_API_KEY = "AIzaSyCoH0PdIWl5x_xIrb0V54TB0svAw2i4p3w"
-
